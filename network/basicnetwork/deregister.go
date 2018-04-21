543faba36ccbac581793e0b26630662275314a98
Tim Schindler <xh3b4sd@gmail.com>
2018-04-21 13:59:59 +0200

    wip

---
+++ network/basicnetwork/deregister.go
@@ -0,0 +1,13 @@
+package basicnetwork
+
+import (
+	"context"
+
+	"github.com/the-anna-project/the-anna-project/action"
+	"github.com/the-anna-project/the-anna-project/node"
+)
+
+// TODO implement node and action deregistration
+func (o *Object) Deregister(ctx context.Context, n node.Interface, a action.Interface) error {
+	return nil
+}
